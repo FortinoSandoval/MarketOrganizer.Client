bc99d55258be19c2be5ae7a8993281ea563913fe
Fortino Sandoval <fortino.sandoval@maritz.com>
2019-04-02 13:46:20 -0700

    login/header/home and router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.component';
 
 import { AppComponent } from './app.component';
 import { ItemService } from './_services/item.service';
 import { ItemsComponent } from './items/items.component';
 import { ItemComponent } from './item/item.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       ItemsComponent,
-      ItemComponent
+      ItemComponent,
+      HeaderComponent,
+      LoginComponent,
+      HomeComponent
    ],
    imports: [
       BrowserModule,
       ReactiveFormsModule,
       FormsModule,
-      HttpClientModule
+      HttpClientModule,
+      AppRoutingModule
    ],
    providers: [
       ItemService
